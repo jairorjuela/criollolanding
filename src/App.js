1dde13b3b7b9e60bd52370fa40ed2a8e2b56a0a2
jairorjuela <genius85862@gmail.com>
2019-07-18 00:02:58 -0500

    Add Bootstrap, navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Test from './components/Test';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Test />
+    </Fragment>
   );
-}
-
+};
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useState } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+export const Test = () => {
+  return (
+    <Fragment>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        Holiiii
+      </Navbar.Brand>
+    </Navbar>
+    </Fragment>
+  );
+};
+export default Test;
