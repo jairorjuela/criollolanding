8ec280b676cc21fc51611ff73c08d3349b98fe14
jairorjuela <genius85862@gmail.com>
2019-07-18 23:28:17 -0500

    Try css, navbar

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,14 +1,20 @@
 import React, { Fragment, useState } from 'react';
-import { Navbar } from 'react-bootstrap';
-
+import { Navbar, Nav } from 'react-bootstrap';
+import './Test.css';
 
 export const Test = () => {
   return (
     <Fragment>
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        Holiiii
+    <Navbar bg="dark" variant="dark" className="navbar">
+      <Navbar.Brand href="#home" className="navbar-brand">
+        CriolloSomosTodos
       </Navbar.Brand>
+      <Nav>
+        <Nav.Link className="navbar-link">More deets</Nav.Link>
+        <Nav.Link className="navbar-link" eventKey={2}>
+          Dank memes
+        </Nav.Link>
+      </Nav>
     </Navbar>
     </Fragment>
   );
