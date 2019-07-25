ea83a78eeaf3c8d34021cc37c6640e447f0dce59
jairorjuela <genius85862@gmail.com>
2019-07-24 23:53:39 -0500

    styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 
 const Main = () => (
-    <Fragment>
-        <Switch>
-            <Route exact path="/" component={Home} />
-        </Switch>
-    </Fragment>
+  <Fragment>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </Fragment>
 );
 
 export default Main;
