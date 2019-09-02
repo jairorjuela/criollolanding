9f63052ee69247f2855704d07bdb5b0614967a88
jairorjuela <genius85862@gmail.com>
2019-09-02 08:57:51 -0500

    deploy

---
+++ src/components/Back1.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Back1.css';
+
+const Back1 = () => (
+    <div className="b1">
+      <h1>holi1</h1>
+    </div>
+);
+
+export default Back1;
---
+++ src/components/Back2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Back2.css';
+
+const Back2 = () => (
+    <div className="b2">
+    <h1>holi2</h1>
+    </div>
+);
+
+export default Back2;
