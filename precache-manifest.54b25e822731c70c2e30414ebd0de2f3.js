f2b9fd922110f9fc509b0d4ad5d49dc9fd4ffc48
jairorjuela <genius85862@gmail.com>
2019-09-03 10:13:01 -0500

    Updates

---
+++ precache-manifest.54b25e822731c70c2e30414ebd0de2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b884312d9cc4b822f605986bd20bf40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a442c348b4dc1555d1f",
+    "url": "/static/css/main.0ea71480.chunk.css"
+  },
+  {
+    "revision": "146e985d09a06393d907",
+    "url": "/static/js/2.e0445f31.chunk.js"
+  },
+  {
+    "revision": "1a442c348b4dc1555d1f",
+    "url": "/static/js/main.bf245b48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
